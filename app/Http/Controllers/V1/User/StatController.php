4a186393053bae856913008b218eaf6d9d252cd5
studycloud111 <74172237+studycloud111@users.noreply.github.com>
2025-07-09 10:21:02 +0800

    流量查询为最近30天

--- app/Http/Controllers/V1/User/StatController.php
+++ app/Http/Controllers/V1/User/StatController.php
@@ -19,7 +19,7 @@ class StatController extends Controller
             'server_rate'
         ])
             ->where('user_id', $request->user['id'])
-            ->where('record_at', '>=', strtotime(date('Y-m-1')))
+            ->where('record_at', '>=', strtotime('-30 days'))
             ->orderBy('record_at', 'DESC');
         return response([
             'data' => $builder->get()
